1239165178a5ab5b247a091ca158787f2e1b7210
Jack <jacknovak.jn@gmail.com>
2023-08-14 10:28:17 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, url_for, redirect
+import datetime
+
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from flask_bootstrap import Bootstrap5
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'ug56u35gu456g73uf6'
+Bootstrap5(app)
+
+# CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
+db = SQLAlchemy()
+db.init_app(app)
+
+
+ckeditor = CKEditor(app)
+
+
+class Recipe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    subtitle = db.Column(db.String(250), nullable=False)
+    date = db.Column(db.String(250), nullable=False)
+    body = db.Column(db.Text, nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
+
+
+class NewRecipe(FlaskForm):
+    title = StringField('Title', validators=[DataRequired()])
+    subtitle = StringField('Subtitle', validators=[DataRequired()])
+    img_url = StringField('Image URL', validators=[DataRequired()])
+    body = CKEditorField('Body')
+    submit = SubmitField('Submit')
+
+
+@app.route('/')
+def home():
+    blah = db.session.execute(db.select(Recipe))
+    posts = blah.scalars().all()
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route('/show-post/<post_id>', methods=['GET', 'POST'])
+def show_post(post_id):
+    requested_post = db.get_or_404(Recipe, post_id)
+    return render_template("post.html", post=requested_post)
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
